17b8b2b12c2d23b1ac1471d68e090fa473476abc
hanafa01 <hana_fakhouri@hotmail.com>
2021-01-28 20:10:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");
+        }else{
+            $(".navbar").removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $(".scroll-up-btn").addClass("show");
+        }else{
+            $(".scroll-up-btn").addClass("show");
+        }
+    });
+
+    $(".scroll-up-btn").click(function(){
+        $('html').animate({scrollTop: 0});
+    });    
+
+    /* toggle menu */
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    }); 
+});
